e9b5e3205880eea159201125e6db4eaf7e8563dc
yosepjeon <enekelx1@naver.com>
2021-03-01 22:28:47 +0900

    도메인 설계 완료

---
+++ src/main/java/com/yosep/jpa/dataJpa/data/entity/Member.java
@@ -0,0 +1,27 @@
+package com.yosep.jpa.dataJpa.data.entity;
+
+import com.yosep.jpa.dataJpa.data.vo.Address;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Setter
+@Getter
+public class Member {
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
+}
---
+++ src/main/java/com/yosep/jpa/dataJpa/data/entity/Movie.java
@@ -0,0 +1,17 @@
+package com.yosep.jpa.dataJpa.data.entity;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("M")
+@Getter
+@Setter
+public class Movie extends Item{
+    private String director;
+    private String actor;
+}
