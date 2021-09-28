6fd77210e4f81be79d014d33b0733c16c38a453d
yosepjeon <enekelx1@naver.com>
2021-09-28 16:35:54 +0900

    지연 로딩과 조회 성능 최적화 Dto 적용

--- src/main/java/com/yosep/jpa/dataJpa/api/OrderApiController.java
+++ src/main/java/com/yosep/jpa/dataJpa/api/OrderApiController.java
@@ -1,14 +1,19 @@
 package com.yosep.jpa.dataJpa.api;
 
+import com.yosep.jpa.dataJpa.common.data.vo.Address;
 import com.yosep.jpa.dataJpa.order.data.entity.Order;
 import com.yosep.jpa.dataJpa.order.data.entity.OrderItem;
+import com.yosep.jpa.dataJpa.order.data.vo.OrderStatus;
 import com.yosep.jpa.dataJpa.order.repository.OrderRepository;
 import com.yosep.jpa.dataJpa.order.repository.OrderRepositorySupport;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -28,5 +33,48 @@ public class OrderApiController {
         }
 
         return all;
+    }
+
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> orderV2() {
+        return orderRepositorySupport.findAll()
+                .stream()
+                .map(OrderDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Data
+    static class OrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItemDto> orderItems;
+
+        public OrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+            orderItems = order.getOrderItems()
+                    .stream()
+                    .map(OrderItemDto::new)
+                    .collect(Collectors.toList());
+        }
+    }
+
+    @Data
+    static class OrderItemDto {
+        private String itemName;
+        private int orderPrice;
+        private int count;
+
+        public OrderItemDto(OrderItem orderItem) {
+            itemName = orderItem.getItem().getName();
+            orderPrice = orderItem.getOrderPrice();
+            count = orderItem.getCount();
+        }
     }
 }
